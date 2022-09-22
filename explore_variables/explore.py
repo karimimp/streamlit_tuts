bdc6466b0022aa2a53bfb366507e6a4c7700044a
Mohsen <mohsenk@outlook.com>
2022-09-22 19:27:55 +0200

    fixing the loop

--- explore_variables/explore.py
+++ explore_variables/explore.py
@@ -12,13 +12,24 @@ selected_variables = Ae_form.multiselect("Select the variables to explore", ["Nu
 data = {}
 c1, c2 = Ae_form.columns(2)
 for var in selected_variables:
+    # key_min = var+"_min"
+    # key_max = var+"_max"
 
-    c1.number_input(label = var+" minimum", format='%.0f')
-    c2.number_input(label = var+" maximum", format='%.0f')
+    min_val = c1.number_input(label = var+" minimum", format='%.0f')
+    max_val = c2.number_input(label = var+" maximum", format='%.0f')
 
+    data[var] = np.linspace(min_val, max_val, num=10)
+
+for key, value in data.items():
+    st.write(f"{key} = {data[key]}")
 # data = {"Number of discs": 1, "Cone angle":1, "Bowl speed":1}
-# df = pd.DataFrame.from_dict(data, orient='index')
+# df = pd.DataFrame.from_dict(data, columns=selected_variables)
+# columns_names = [col_name for col_name in selected_variables]
+# st.write(columns_names)
 
+# for var in selected_variables:
+#     st.write(var)
+# st.write(data)
 
 # c1.write("column1")
 
